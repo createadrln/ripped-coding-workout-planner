e84ddd27bb882331994e90d86d9c9bd136b6703e
Ryan MacMurray <macmurray_ryan@bah.com>
2020-04-24 07:40:29 -0400

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './_services/auth.service';
 
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { NotebooksComponent } from './components/workouts/notebooks/notebooks.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'notebooks', component: NotebooksComponent },
+  // { path: 'login-email', component: EmailComponent },
+  // { path: 'settings', component: MembersComponent, canActivate: [AuthGuard] },
+  // { path: 'notebook', component: NotesComponent },
+  // { path: 'notes', component: WorkoutNotesComponent },
+  // { path: 'terms-and-conditions', component: TermsAndConditionsComponent },
+  // { path: 'privacy-policy', component: PrivacyComponent },
+  // { path: 'rules', component: RulesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
