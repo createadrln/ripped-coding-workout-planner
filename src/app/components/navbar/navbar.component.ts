3a258ba6772e50f3978f3e14a96217f2276c6cdd
Ryan MacMurray <macmurray_ryan@bah.com>
2020-04-23 20:16:38 -0400

    Add navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/auth';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  navbarOpen = false;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private afAuth: AngularFireAuth
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit() { }
+
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
   }
 
+  logout() {
+    this.afAuth.signOut();
+    this.router.navigateByUrl('/login');
+    this.toggleNavbar();
+  }
 }
