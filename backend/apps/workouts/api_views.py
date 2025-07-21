24fbb136734f8fa6752d129433f9517344a08277
Ryan MacMurray <ryan@createadrenaline.com>
2025-07-21 06:55:15 -0400

    Formatting

--- backend/apps/workouts/api_views.py
+++ backend/apps/workouts/api_views.py
@@ -2,20 +2,24 @@ from rest_framework import viewsets, permissions
 from .models import Exercise, Workout, WorkoutTemplate, Set
 from .serializers import ExerciseSerializer, WorkoutSerializer, WorkoutTemplateSerializer, SetSerializer
 
+
 class ExerciseViewSet(viewsets.ModelViewSet):
     queryset = Exercise.objects.all()
     serializer_class = ExerciseSerializer
     permission_classes = [permissions.IsAuthenticated]
+
 
 class WorkoutTemplateViewSet(viewsets.ModelViewSet):
     queryset = WorkoutTemplate.objects.all()
     serializer_class = WorkoutTemplateSerializer
     permission_classes = [permissions.IsAuthenticated]
 
+
 class WorkoutViewSet(viewsets.ModelViewSet):
     queryset = Workout.objects.all()
     serializer_class = WorkoutSerializer
     permission_classes = [permissions.IsAuthenticated]
+
 
 class SetViewSet(viewsets.ModelViewSet):
     queryset = Set.objects.all()
